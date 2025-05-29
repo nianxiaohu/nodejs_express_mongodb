dbc6ee5b47e6542091f1c6401cc447fca75de97b
Xiaohu Nian <xiaohunian@gmail.com>
2025-05-28 21:28:47 -0700

    Fix some assets path in html

--- ch03/index.js
+++ ch03/index.js
@@ -4,29 +4,36 @@ const path_2 = path;
 const app = new express();
 const test_flag = true;
 
-app.use('/static',express.static('public'));
+app.use(express.static('public'));
 // Serving multiple static folders, /??/
 app.use('/static_',express.static('startbootstrap-clean-blog-gh-pages'));
 
 app.listen(4000, () => {
     console.log('App listening on port 4000');
 });
-
 app.get('/', (req, res) => {
     if (test_flag ==true){
         res.sendFile(path.resolve(__dirname, 'pages_xhu_test/index.html'));
     }
     else {res.sendFile(path.resolve(__dirname, 'pages/index.html'));}
 });
-
 app.get('/about', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'pages/about.html'))
+      if (test_flag ==true){
+        res.sendFile(path.resolve(__dirname, 'pages_xhu_test/about.html'));
+    }
+    else {res.sendFile(path.resolve(__dirname, 'pages/about.html'));}
 });
 
 app.get('/contact', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'pages/contact.html'))
+      if (test_flag ==true){
+        res.sendFile(path.resolve(__dirname, 'pages_xhu_test/contact.html'));
+    }
+    else {res.sendFile(path.resolve(__dirname, 'pages/contact.html'));}
 });
 
 app.get('/post', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'pages/post.html'))
+       if (test_flag ==true){
+        res.sendFile(path.resolve(__dirname, 'pages_xhu_test/post.html'));
+    }
+    else {res.sendFile(path.resolve(__dirname, 'pages/post.html'));}
 });
