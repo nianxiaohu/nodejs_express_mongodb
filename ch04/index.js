b5185e9ff8971c9c438e2d25b522dcc18f8f4fb4
Xiaohu Nian <xiaohunian@gmail.com>
2025-06-03 23:48:52 -0700

    Embedded javascript Templating

--- ch04/index.js
+++ ch04/index.js
@@ -10,19 +10,19 @@ app.use(express.static('public'));
 app.listen(4000, () => {
     console.log('App listening on port 4000');
 });
-
+const user={name:'xiaohu'};
 app.get('/', (req, res) => {
-    res.render('index');
+    res.render('index',{'user':user});
 });
 
 app.get('/about', (req, res) => {
-    res.render('about');
+    res.render('about',{'user':user});
 });
 
 app.get('/contact', (req, res) => {
-    res.render('contact');
+    res.render('contact',{'user':user});
 });
 
 app.get('/post', (req, res) => {
-    res.render('post');
+    res.render('post',{'user':user});
 });
