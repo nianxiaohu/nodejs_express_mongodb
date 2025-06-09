dec0cc429e4b53fd83d021f5aef37c5943ecc5c4
Xiaohu Nian <xiaohunian@gmail.com>
2025-06-08 17:45:24 -0700

    mongoose---

--- ch04/mongodb_first.js
+++ ch04/mongodb_first.js
@@ -1,5 +1,6 @@
 const { MongoClient } = require('mongodb');
 const mongoose = require('mongoose');
+const { Schema , model} = mongoose;
 // or as an es module:
 // import { MongoClient } from 'mongodb'
 
@@ -23,6 +24,8 @@ async function main() {
   tags: ['cotton'],
   size: { h: 28, w: 35.5, uom: 'cm' }
 }); */
+//await db.collection('animals').insertOne({_id:2,name:'mimi',type:'dog'});
+
 
 /* 
 // By default, Mongoose adds an _id property to your schemas
@@ -35,21 +38,22 @@ console.log(doc._id instanceof mongoose.Types.ObjectId);
 
 
 
-
+await mongoose.connect('mongodb://127.0.0.1:27017/myProject');
 // You can also overwrite Mongoose's default _id with your own _id. Just be careful: Mongoose will refuse to save a top-level document that doesn't have an _id, so you're responsible for setting _id if you define your own _id path.
-const schema = new mongoose.Schema({
+const schema = new Schema({
   _id: Number, // <-- overwrite Mongoose's default `_id`
  name: String,
  type: String,
 });
-const Model = mongoose.model('Animal', schema);
+const Model = model('Animal', schema);
 const doc = new Model();
 //await doc.save(); // Throws "document must have an _id before saving"
-doc._id = 3;
-doc.name='andy';
-doc.type='dog';
+doc._id = 6;
+doc.name='roger';
+doc.type='rabbit';
 await doc.save();
-//await db.collection('animals').insertOne({_id:2,name:'mimi',type:'dog'});
+console.log("Document saved!"); // Executes after the save
+
 
 
 //await doc.save(); // works  buffering timed out after 10000ms
@@ -58,10 +62,10 @@ await doc.save();
 
 
 
-  //return 'done.';
+  return 'done.';
 }
 
 main()
   .then(console.log)
   .catch(console.error)
-  .finally(() => client.close());
+  .finally(() => {client.close(); mongoose.disconnect();});
