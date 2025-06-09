2efa6725aff2ac46238c1988ae4200c726f3c73d
Xiaohu Nian <xiaohunian@gmail.com>
2025-06-09 00:29:04 -0700

    instance method for schema

--- ch04/mongodb_first.js
+++ ch04/mongodb_first.js
@@ -39,7 +39,8 @@ console.log(doc._id instanceof mongoose.Types.ObjectId);
 
 
 await mongoose.connect('mongodb://127.0.0.1:27017/myProject');
-// You can also overwrite Mongoose's default _id with your own _id. Just be careful: Mongoose will refuse to save a top-level document that doesn't have an _id, so you're responsible for setting _id if you define your own _id path.
+
+/* // You can also overwrite Mongoose's default _id with your own _id. Just be careful: Mongoose will refuse to save a top-level document that doesn't have an _id, so you're responsible for setting _id if you define your own _id path.
 const schema = new Schema({
   _id: Number, // <-- overwrite Mongoose's default `_id`
  name: String,
@@ -52,10 +53,35 @@ doc._id = 6;
 doc.name='roger';
 doc.type='rabbit';
 await doc.save();
-console.log("Document saved!"); // Executes after the save
+console.log("Document saved!"); // Executes after the save */
 
 
+//Instance methods
+//Instances of Models are documents. Documents have many of their own built-in instance methods. We may also define our own custom document instance methods.
+// define a schema
+const animalSchema = new Schema({ name: String, type: String },
+  {
+  // Assign a function to the "methods" object of our animalSchema through schema options.
+  // By following this approach, there is no need to create a separate TS type to define the type of the instance functions.
+    methods: {
+      findSimilarTypes(cb) {
+        return mongoose.model('Animal').find({ type: this.type }, cb);
+      }
+    }
+  });
 
+/* // Or, assign a function to the "methods" object of our animalSchema
+animalSchema.methods.findSimilarTypes = function(cb) {
+  return mongoose.model('Animal').find({ type: this.type }, cb);
+}; */
+const Animal = mongoose.model('Animal', animalSchema);
+const dog = new Animal({ type: 'dog', name:'mongoose_dog' });
+const wolf = new Animal({type:'dog',name:'seal_dog'});
+await wolf.save();
+await dog.save();
+
+
+console.log(dog.findSimilarTypes());
 //await doc.save(); // works  buffering timed out after 10000ms
 
 
