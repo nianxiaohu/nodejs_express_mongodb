6a9a0d1be424a89dac28c4527efc1a990b768fed
Xiaohu Nian <xiaohunian@gmail.com>
2025-06-24 22:27:45 -0700

    Chapter10 fix storeUser.js

--- ch10-11-12/controllers/loginUser.js
+++ ch10-11-12/controllers/loginUser.js
@@ -3,19 +3,17 @@ const User = require('../models/User');
 
 module.exports = (req, res) => {
     const { username, password } = req.body;
-    User.findOne({username: username}, (error, user) => {
-        if (user) {
-            bcrypt.compare(password, user.password, (error, same) => {
-                if (same) { // if passwords match
-                    console.log(user);
-                    req.session.userId = user._id;
-                    res.redirect('/');
-                } else {
-                    res.redirect('/auth/login');
-                }
-            });
-        } else {
-            res.redirect('/auth/login');
-        }
-    });
+    User.findOne({ username: username }).then(user => {
+        bcrypt.compare(password, user.password, (error, same) => {
+            if (same) { // if passwords match
+                console.log(user);
+                req.session.userId = user._id;
+                res.redirect('/');
+            } else {
+                res.redirect('/auth/login');
+            }
+        });
+    }).catch(error => {
+        res.redirect('/auth/login');
+    })
 };
--- ch10-11-12/controllers/storeUser.js
+++ ch10-11-12/controllers/storeUser.js
@@ -2,13 +2,17 @@ const User = require('../models/User.js');
 const path = require('path');
 
 module.exports = (req, res) => {
-    User.create(req.body).then((error, user) => {
-        if (error) {
-            const validationErrors = Object.keys(error.errors).map(key => error.errors[key].message)
-            req.flash('validationErrors', validationErrors)
-            req.flash('data', req.body)
-            return res.redirect('/auth/register')
-        }
+    User.create(req.body).then(newUser => {
+        console.log(newUser);
         res.redirect('/');
+    }).catch(error => {
+        console.log("Error here");
+        console.log(error);
+        /*temp solution for error TypeError: Cannot convert undefined or null to object
+at Object.keys (<anonymous>)*/
+        //const validationErrors = Object.keys(error.errors).map(key => error.errors[key].message)
+        //req.flash('validationErrors', validationErrors)
+        //req.flash('data', req.body)
+        return res.redirect('/auth/register');
     })
 };
