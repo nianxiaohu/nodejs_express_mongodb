779aadbb5c922ad05ddaefa8b60b40c2a7eac88a
Xiaohu Nian <xiaohunian@gmail.com>
2025-07-04 15:07:02 -0700

    errors notification

--- ch10-11-12/controllers/storeUser.js
+++ ch10-11-12/controllers/storeUser.js
@@ -6,13 +6,23 @@ module.exports = (req, res) => {
         console.log(newUser);
         res.redirect('/');
     }).catch(error => {
-        console.log("Error here");
-        console.log(error);
         /*temp solution for error TypeError: Cannot convert undefined or null to object
 at Object.keys (<anonymous>)*/
-        //const validationErrors = Object.keys(error.errors).map(key => error.errors[key].message)
-        //req.flash('validationErrors', validationErrors)
-        //req.flash('data', req.body)
+        const validationErrors = Object.keys(error.errors).map(key => error.errors[key].message);
+        req.flash('validationErrors', validationErrors);
+        req.flash('data', req.body);
+        console.log("error_xiaohu")
+        console.log(Object.getOwnPropertyNames(error));
+        console.log("error.errors_xiaohu")
+        console.log(Object.getOwnPropertyNames(error.errors));
+        console.log(Object.getOwnPropertyNames(error.errors['username']));
+        const keys = Object.getOwnPropertyNames(error.errors['username']);
+        for (let i = 0; i < keys.length; i++) {
+            console.log("xiaohunian");
+            console.log(keys[i]);
+            console.log(error.errors['username'][keys[i]]);
+        }
+        ;
         return res.redirect('/auth/register');
     })
 };
