61bfc5fbb7f8af04954407af92602b059da0c08c
Xiaohu Nian <xiaohunian@gmail.com>
2025-07-05 11:58:02 -0700

    To be finished?

--- ch10-11-12/controllers/home.js
+++ ch10-11-12/controllers/home.js
@@ -1,7 +1,14 @@
 const BlogPost = require('../models/BlogPost.js');
-
+const User = require('../models/User.js');
 module.exports = async (req, res) => {
     const blogposts = await BlogPost.find({});
+    for (let i = 0; i < blogposts.length; i++) {
+        if (typeof blogposts[i].userid != "undefined") {
+            console.log("home.js");
+            console.log(typeof blogposts[i].userid);
+            console.log(Object.keys(blogposts[i].userid));
+        }
+    }
     console.log(req.session);
     res.render('index', {
         blogposts
