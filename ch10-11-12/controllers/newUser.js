ab8b5039c975922354b4bfb9efd38c9babaebcdd
Xiaohu Nian <xiaohunian@gmail.com>
2025-07-04 17:51:31 -0700

    debugging flash

--- ch10-11-12/controllers/newUser.js
+++ ch10-11-12/controllers/newUser.js
@@ -1,14 +1,18 @@
-module.exports = (req, res) =>{ 
-    var username = "" 
+module.exports = (req, res) => {
+    var username = ""
     var password = ""
-    const data = req.flash('data')[0];    
+    const data = req.flash('data')[0];
+    /* console.log('newUser.js');
+    console.log(data);
+    console.log("Read Again");
+    console.log(req.flash('data')); */
 
-    if(typeof data != "undefined"){        
+    if (typeof data != "undefined") {
         username = data.username
         password = data.password
     }
-     
-    res.render('register',{        
+
+    res.render('register', {
         errors: req.flash('validationErrors'),
         username: username,
         password: password
